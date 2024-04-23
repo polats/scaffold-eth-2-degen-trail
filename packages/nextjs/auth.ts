dc536ea056247f84f060c40c63de4a0661313303
Paul Gadi <paulgadi@gmail.com>
2024-04-23 15:40:57 -0400

    add nextauth route

---
+++ packages/nextjs/auth.ts
@@ -0,0 +1,66 @@
+import { headers } from "next/headers";
+import { IncomingHttpHeaders } from "http";
+import Credentials from "next-auth/providers/credentials";
+import { getCsrfToken } from "next-auth/react";
+import { SiweMessage } from "siwe";
+
+export const authOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+
+  providers: [
+    Credentials({
+      name: "Ethereum",
+
+      credentials: {
+        message: {
+          label: "Message",
+          placeholder: "0x0",
+          type: "text",
+        },
+        signature: {
+          label: "Signature",
+          placeholder: "0x0",
+          type: "text",
+        },
+      },
+
+      async authorize(credentials) {
+        try {
+          const siwe = new SiweMessage(JSON.parse(credentials?.message || "{}"));
+
+          const nextAuthUrl =
+            process.env.NEXTAUTH_URL || (process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : null);
+          if (!nextAuthUrl) {
+            return null;
+          }
+
+          const nextAuthHost = new URL(nextAuthUrl).host;
+          if (siwe.domain !== nextAuthHost) {
+            return null;
+          }
+
+          const reqHeader = headers() as unknown as IncomingHttpHeaders;
+          if (siwe.nonce !== (await getCsrfToken({ req: { headers: reqHeader } }))) {
+            return null;
+          }
+
+          await siwe.verify({ signature: credentials?.signature || "" });
+          return {
+            id: siwe.address,
+          };
+        } catch (e) {
+          return null;
+        }
+      },
+    }),
+  ],
+
+  callbacks: {
+    async session({ session, token }: { session: any; token: any }) {
+      if (!session.sub) {
+        session.sub = token.sub;
+      }
+      return session;
+    },
+  },
+};
